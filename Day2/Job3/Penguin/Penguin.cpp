62005565e08370dfe09a3ff79a5012e232430db4
Clément <clement.marin@laplateforme.io>
2024-04-03 14:51:28 +0200

    [ADD] Day2-Job3 test

--- Day2/Job3/Penguin/Penguin.cpp
+++ Day2/Job3/Penguin/Penguin.cpp
@@ -7,7 +7,9 @@
 std::vector<std::weak_ptr<Penguin>> Penguin::allPenguins;
 
 Penguin::Penguin(std::string name, double swimSpeed, double walkSpeed, double slideSpeed)
-        : Aquatic(swimSpeed), Terrestrial(walkSpeed), _name(std::move(name)), _slideSpeed(slideSpeed) {}
+        : Aquatic(swimSpeed), Terrestrial(walkSpeed), _name(std::move(name)), _slideSpeed(slideSpeed) {
+    allPenguins.push_back(self);
+}
 
 Penguin::Penguin(const Penguin &other)
         : Aquatic(other._swimSpeed), Terrestrial(other._walkSpeed), _name(other._name),
@@ -50,11 +52,22 @@ void Penguin::cleanUp() {
 int main() {
     std::shared_ptr<Penguin> penguin1 = std::make_shared<Penguin>("Penguin1", 2.0, 1.0, 3.0);
     std::shared_ptr<Penguin> penguin2 = std::make_shared<Penguin>("Penguin2", 3.0, 2.0, 4.0);
+
     penguin1->introduce();
     penguin1->swim();
     penguin1->walk();
     penguin2->introduce();
     penguin2->swim();
     penguin2->walk();
+
+    std::cout << "Number of penguins: " << Penguin::allPenguins.size() << std::endl;
+
+    // reset penguin1
+    penguin1.reset();
+    // reset penguin2
+    penguin2.reset();
+
+    // check size of allPenguins
+    std::cout << "Number of penguins: " << Penguin::allPenguins.size() << std::endl;
     return 0;
 }
