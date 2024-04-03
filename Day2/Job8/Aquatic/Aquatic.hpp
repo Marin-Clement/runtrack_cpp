50a21c9c486da8555bf519566ce3c4972fdb75b0
Clément <clement.marin@laplateforme.io>
2024-04-03 15:02:42 +0200

    [ADD] Day2-Job8

---
+++ Day2/Job8/Aquatic/Aquatic.hpp
@@ -0,0 +1,19 @@
+//
+// Created by Clément on 03/04/2024.
+//
+
+#ifndef RUNTRACK_CPP_AQUATIC_HPP
+#define RUNTRACK_CPP_AQUATIC_HPP
+
+
+class Aquatic {
+protected:
+    double _swimSpeed;
+public:
+    explicit Aquatic(double swimSpeed) : _swimSpeed(swimSpeed) {}
+
+    virtual void swim() = 0;
+};
+
+
+#endif //RUNTRACK_CPP_AQUATIC_HPP
